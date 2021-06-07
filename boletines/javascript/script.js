0bf9fecba1c9181a114576f4fe5f15e709517151
Fernando Mare <fernando.mare03@gmail.com>
2021-06-07 19:00:24 -0300

    Ahora si agregamos el proyecto

---
+++ boletines/javascript/script.js
@@ -0,0 +1,24 @@
+let links = document.querySelectorAll(".close");
+
+links.forEach(function (link) {
+    link.addEventListener('click', function (ev) {
+        ev.preventDefault();
+
+        let content = document.querySelector('.content');
+
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+
+        setTimeout(function () {
+            location.href = "/";
+        }, 600);
+
+
+        return false;
+    })
+
+});
